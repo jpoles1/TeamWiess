c27ab67f427696ef6ac01c3890176a32110dbdd9
Jordan Poles <jpoles1@gmail.com>
2015-06-13 10:51:39 -0500

    Added support for google ajax crawling, and added a sitemap.

--- js/nav.js
+++ js/nav.js
@@ -1,26 +1,46 @@
 /**
  * Created by jordan on 6/5/15.
  */
-var router = {"#home": "home.html", "#about": "about.html", "#newstudents": "newstudents.html", "#cabinet": "cabinet.html", "#court": "court.html", "#reps": "reps.html", "#resources": "resources.html", "#calendar": "calendar.html"};
-loadContent = function() {
-    loc = window.location.hash;
-    selectButton();
-    if(router[loc]!=undefined){
-        $("#main").load(router[loc]);
+var router = {"#!home": "home.html", "#!about": "about.html", "#!newstudents": "newstudents.html", "#!cabinet": "cabinet.html", "#!court": "court.html", "#!reps": "reps.html", "#!resources": "resources.html", "#!calendar": "calendar.html"};
+urlParam = function(theParameter) {
+    var params = window.location.search.substr(1).split('&');
+    for (var i = 0; i < params.length; i++) {
+        var p=params[i].split('=');
+        if (p[0] == theParameter) {
+            return decodeURIComponent(p[1]);
+        }
     }
-    else{
+    return false;
+};
+loadContent = function() {
+    var crawlurl = urlParam("_escaped_fragment_");
+    if (crawlurl == false) {
+        loc = window.location.hash;
+    }
+    else {
+        loc = "#!"+crawlurl;
+    }
+    selectButton();
+    if (router[loc] != undefined) {
+        $("#main").load(router[loc], function () {
+            if (loc == "#!cabinet" || loc == "#!reps") {
+                $('[data-toggle="tooltip"]').tooltip()
+            }
+        });
+    }
+    else {
         $("#home").siblings("li").removeClass("active");
         $("#home").addClass("active");
-        $("#main").load("home.html", function(){
-            var numpics =25;
+        $("#main").load("home.html", function () {
+            var numpics = 25;
             for (i = 0; i < numpics; i++) {
-                if(i==0){
-                    $("#photoindc").append("<li data-target='#carousel' data-slide-to='"+i+"' class='active'></li>")
-                    $("#photos").append("<div class='item active'><img src='photos/"+i+".jpg' class='img-responsive'/></div>");
+                if (i == 0) {
+                    $("#photoindc").append("<li data-target='#carousel' data-slide-to='" + i + "' class='active'></li>")
+                    $("#photos").append("<div class='item active'><img src='photos/" + i + ".jpg' class='img-responsive'/></div>");
                 }
-                else{
-                    $("#photoindc").append("<li data-target='#carousel' data-slide-to='"+i+"'></li>")
-                    $("#photos").append("<div class='item'><img src='photos/"+i+".jpg' class='img-responsive'/></div>");
+                else {
+                    $("#photoindc").append("<li data-target='#carousel' data-slide-to='" + i + "'></li>")
+                    $("#photos").append("<div class='item'><img src='photos/" + i + ".jpg' class='img-responsive'/></div>");
                 }
 
             }
