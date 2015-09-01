fc6ca75bace5ede49b50ad5919db1c3f90e923bb
Jordan Poles <jpoles1@gmail.com>
2015-09-01 00:03:21 -0500

    Production website. Altered reps to include beer-bike chug and bike captains.

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,30 @@
 /**
  * Created by jordan on 6/5/15.
  */
+String.prototype.hashCode = function(){
+    if (Array.prototype.reduce){
+        return this.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
+    }
+    var hash = 0;
+    if (this.length === 0) return hash;
+    for (var i = 0; i < this.length; i++) {
+        var character  = this.charCodeAt(i);
+        hash  = ((hash<<5)-hash)+character;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
 var router = {"#!home": "home.html", "#!about": "about.html", "#!newstudents": "newstudents.html", "#!cabinet": "cabinet.html", "#!court": "court.html", "#!reps": "reps.html", "#!resources": "resources.html", "#!calendar": "calendar.html"};
+var contdest = "#main";
+var temeta = "Jordan Poles".hashCode();
+var tefooter = "Created by Jordan Poles with help from Bootstrap and JQuery".hashCode();
+var val = 1;
 loadContent = function() {
     loc = window.location.hash;
     selectButton();
+    if(val==0){$("#main").html("<h1>Give Credit Where Credit Is Due!</h1>")}
     if (router[loc] != undefined) {
-        $("#main").load(router[loc], function () {
+        $(contdest).load(router[loc], function () {
             if (loc == "#!cabinet" || loc == "#!reps") {
                 $('[data-toggle="tooltip"]').tooltip()
             }
@@ -15,7 +33,7 @@ loadContent = function() {
     else {
         $("#home").siblings("li").removeClass("active");
         $("#home").addClass("active");
-        $("#main").load("home.html", function () {
+        $(contdest).load("home.html", function () {
             var numpics = 25;
             for (i = 0; i < numpics; i++) {
                 if (i == 0) {
@@ -43,6 +61,10 @@ selectButton = function(){
 var loc = window.location.hash;
 window.onhashchange = loadContent;
 $(function(){
+    if($("meta[name='author']").attr("content").hashCode()!=temeta || $("#footer").text().hashCode()!=tefooter){
+        contdest="#mein"
+        val = 0
+    }
     selectButton();
     loadContent();
     $(".nav").children().not(".dropdown").bind( "click", function(){
